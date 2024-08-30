ef5f021b618d283b569cf98b0fdb2d85fe8a1e6f
unknown <qureshinasra47@gmail.com>
2024-08-30 18:04:46 +0530

    msg

--- src/component/layout/header.js
+++ src/component/layout/header.js
@@ -1,42 +1,96 @@
+import React from 'react'
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import Carousel from 'react-bootstrap/Carousel';
+import Logo from '../../assets/images.jpeg';
 
-const header = () => {
+
+function header() {
   return (
     <>
       <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-      <Navbar.Brand>
-            <img
-              alt=""
-              src="./assets/images.jpeg"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-            />{' '}
-            React Bootstrap
-          </Navbar.Brand>
-      
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
+        <Container fluid>
+          <img
+            src={Logo}
+            width="100"
+            height="100"
+            className="d-inline-block align-top"
+            alt="React Bootstrap logo"
+          />
           <Nav
-            className="me-auto my-2 my-lg-0"
+            className=" justify-content-end"
             style={{ maxHeight: '100px' }}
             navbarScroll
           >
-            <Nav.Link href="./component/home">Home</Nav.Link>
-            <Nav.Link href="#action2">about</Nav.Link>
-            <Nav.Link href="#action2">product</Nav.Link>
-            <Nav.Link href="#">contact</Nav.Link>
-      </Nav>
-       </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  </>
+            <Nav.Link href="#action1">HOME</Nav.Link>
+            <Nav.Link href="#action2">ABOUT</Nav.Link>
+            <Nav.Link href="#product">PRODUCT</Nav.Link>
+            <Nav.Link href="#conatct">CONTACT</Nav.Link>
+            <NavDropdown title="Link" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">
+                Something else here
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#" disabled>
+              Link
+            </Nav.Link>
+          </Nav>
+
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Container>
+      </Navbar>
+      <Carousel>
+      <Carousel.Item>
+        <img src={Logo}/>
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <img src={Logo}/>
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <img src={Logo}/>
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+
+    </>
   );
 }
+
+
+
+
 export default header
+
+
+
+
