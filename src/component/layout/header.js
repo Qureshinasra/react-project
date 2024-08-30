b9e4c77ef87880b652408b2ae9b0cd89221e6d29
Qureshinasra <qureshinasra47@gmail.com>
2024-08-30 14:32:32 +0530

    msg

--- src/component/layout/header.js
+++ src/component/layout/header.js
@@ -1,42 +1,53 @@
-import Button from 'react-bootstrap/Button';
+import React from 'react'
 import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import Logo from '../../assets/logo.png';
+
 
 const header = () => {
   return (
-    <>
-      <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-      <Navbar.Brand>
-            <img
-              alt=""
-              src="./assets/images.jpeg"
-              width="30"
-              height="30"
+    <Navbar expand="lg" className="bg-body-tertiary">
+    <Container>
+    <Navbar className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img src={Logo}
+              width="60px"
+              height="60px"
               className="d-inline-block align-top"
-            />{' '}
-            React Bootstrap
+              alt="creative-logo"
+            />
           </Navbar.Brand>
-      
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Nav.Link href="./component/home">Home</Nav.Link>
-            <Nav.Link href="#action2">about</Nav.Link>
-            <Nav.Link href="#action2">product</Nav.Link>
-            <Nav.Link href="#">contact</Nav.Link>
-      </Nav>
-       </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  </>
-  );
+        </Container>
+      </Navbar>
+      <Navbar.Brand href="#home">National Defence Academy</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#aboutus">About Us</Nav.Link>
+          <Nav.Link href="#facility">Facilities</Nav.Link>
+          <Nav.Link href="#contact">Contact</Nav.Link>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+          
+         
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+  )
 }
+
 export default header
